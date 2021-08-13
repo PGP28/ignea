86155d1ce80651ce4bc0886c16f458458c46d4cf
Paul_Guzmán <andres22.pgpa@gmail.com>
2021-08-12 22:38:21 -0400

    Landing-page-2

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -47,12 +47,16 @@ export default function LandingPage(props) {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h1 className={classes.title}>
+                Tu historia comienza con nosotros en Ignea.
+              </h1>
               <h4>
-                Every landing page needs a small description after the big bold
+                {/* Every landing page needs a small description after the big bold
                 title, that{"'"}s why we added this text here. Add here all the
                 information that can make you or your product create the first
-                impression.
+                impression. */}
+                Especialistas en herrería metálica, carpintería metálica,
+                montajes de estructuras, obras civiles metal mecánica.
               </h4>
               <br />
               <Button
